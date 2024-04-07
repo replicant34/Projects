98ad71174ae22fe62d323bf3595d6d4afbd03bc7
Ilia Grishkin <150169630+replicant34@users.noreply.github.com>
2024-04-08 00:28:03 +0100

    Delete Website for Inventory Control directory

--- Website for Inventory Control/partials/app-topNav.php
+++
@@ -1,4 +0,0 @@
-<div class="dashboard_topNav">
-                <a href="" id="toggleBtn"><i class="fa fa-navicon"></i></a>
-                <a href="database/logout.php" id="logoutBtn"><i class="fa fa-power-off"></i>Log out</a>
- </div>
